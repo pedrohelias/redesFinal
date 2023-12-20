373df582a0a05bfbc5394615011f146d8d5d1f41
pedrohelias <pedrohelias95@hotmail.com>
2023-12-20 18:01:10 -0300

    alteracoes

--- webServer.py
+++ webServer.py
@@ -1,33 +1,27 @@
 import asyncio
 import websockets
 
-# Lista para armazenar as conexões dos clientes
 clients = set()
 
 async def register(websocket):
-    # Adiciona a conexão do cliente à lista
+
     clients.add(websocket)
     #print(f"Novo cliente conectado. Total de clientes: {len(clients)}")
     print(f"Novo cliente conectado")
 
 async def unregister(websocket):
-    # Remove a conexão do cliente da lista
     clients.remove(websocket)
-    #print(f"Cliente desconectado. Total de clientes: {len(clients)}")
     print(f"Cliente desconectado")
 
 async def chat(websocket, path):
-    # Registra o novo cliente
+
     await register(websocket)
     try:
-        # Aguarda mensagens do cliente
         async for message in websocket:
-            # Envia a mensagem para todos os clientes conectados
             await asyncio.gather(
                 *[client.send(message) for client in clients if client != websocket]
             )
     finally:
-        # Remove o cliente da lista ao desconectar
         await unregister(websocket)
 
 start_server = websockets.serve(chat, "localhost", 12345)
